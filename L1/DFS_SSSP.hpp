692d4795a57c7209f9d101975a2c19ac939a5b94
Akshaj <67319076+Akshaj-Padmakar@users.noreply.github.com>
2022-10-01 17:14:37 +0530

    .

---
+++ L1/DFS_SSSP.hpp
@@ -0,0 +1,14 @@
+//consider we have the graph g{adjacency list}
+	
+vector<int> dis(n + 1, -1); //stores the distance from the root
+	
+function<void(int, int)> dfs = [&](int node, int par) { // lamda function in CPP
+    dis[node] = dis[par] + 1;
+		for (auto ch : g[node]) {
+			if (ch ^ par) {
+				dfs(ch, node);
+			}
+		}
+};
+
+dfs(1);
